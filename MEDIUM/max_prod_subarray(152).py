36c9833be2d5c522c4714cd127be79fbe854d35a
SAPNIL-M <sapnilm670@gmail.com>
2025-08-02 19:27:07 +0530

    was able to do the brute force within 20min but not able to think about opt

---
+++ MEDIUM/max_prod_subarray(152).py
@@ -0,0 +1,13 @@
+def max_prod_subarray(nums):
+    pre=suf=1
+    ans=float('-inf')
+    n=len(nums)
+    for i in range(n):
+        pre*=nums[i]
+        suf*=nums[n-1-i]
+        ans=max(ans,pre,suf)
+        if pre==0 : pre=1
+        if suf==0 : suf=1
+    return ans
+
+print(max_prod_subarray([2,3,-2,4]))
