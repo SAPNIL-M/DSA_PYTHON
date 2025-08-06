ff0d14e3b55d4d4399706bfc399c4ba23580cb01
SAPNIL-M <sapnilm670@gmail.com>
2025-08-07 00:27:03 +0530

    the solution with better space comp

--- MEDIUM/rotate_array(189).py
+++ MEDIUM/rotate_array(189).py
@@ -1,11 +1,21 @@
-nums=[1,2,3,4,5,6,7]
-n=len(nums)
-k=3
-k=k%n
-ans=[0]*n
-p=-1*(k)
-for i in range(n):
-    ans[i]=nums[p+i]
-for i in range(n):
-    nums[i]=ans[i]
-print(nums)
+def rotate_arr(nums,k):
+    n=len(nums)
+    k=k%n
+    l,r=0,n-1
+    while l<r:
+        nums[l],nums[r]=nums[r],nums[l]
+        l+=1
+        r=r-1
+    l,r=0,k-1
+    while l<r:
+        nums[l],nums[r]=nums[r],nums[l]
+        l+=1
+        r=r-1
+    l,r=k,n-1
+    while l<r:
+        nums[l],nums[r]=nums[r],nums[l]
+        l+=1
+        r=r-1
+    return nums
+
+print(rotate_arr([1,2,3,4,5,6,7,8],3))
